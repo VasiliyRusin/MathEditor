2094a53396dce0e0e8275e2c2f9a5c8f6037ae39
Vasiliy Rusin <rusinva@gmail.com>
2019-04-13 17:28:13 +0700

    I must enjoy torturing myself

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "@vue/prettier"],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "prettier/prettier": {
+            tabWidth: 4
+        }
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+
 };
